27014db53d5cc0da0453bab6cf563f412b5f8a1d
Samson Sham <>
2020-01-07 19:09:31 +0900

    WIP

--- src/App.svelte
+++ src/App.svelte
@@ -3,29 +3,34 @@
 	import { beforeUpdate, afterUpdate } from 'svelte';
 	export let name;
 
+	function setCursor(node, position) {
+		const range = document.createRange();
+		range.setStart(node, position);
+		range.collapse(true);
+		return range;
+	}
 	afterUpdate(() => {
 		const div = document.querySelector("#text");
-		const range = document.createRange();
 		// Children could be text, could be div
 		const lastChild = div.childNodes[div.childNodes.length-1];
 		console.log(div.childNodes, lastChild);
-		// return;
 		if (!lastChild) return;
-		range.setStart(lastChild, lastChild.innerText ? 1 : lastChild.length);	// if it is a div, return 1 for startOffset
-		range.collapse(true);
 		const selection = window.getSelection();
 		selection.removeAllRanges();
-		selection.addRange(range);
+		// if it is a div, return 1 for startOffset
+		selection.addRange(setCursor(lastChild, lastChild.innerText ? 1 : lastChild.length));
 	});
 	let data = {
 		// Uint8Array?
 		_data: [],
 		get hex() {
+			// format for long 
 			return this._data.join(" ");
 		},
 		set hex(newHex) {
 			const SPAN = 2;
-			const sanitizedHex = newHex.replace(/[^0-9a-fA-F]/g, "").replace(new RegExp(`^0{${SPAN-1}}`), "");
+			// const sanitizedHex = newHex.replace(/[^0-9a-fA-F\n]/g, "").replace(new RegExp(`^0{${SPAN-1}}`), "");
+			const sanitizedHex = newHex.replace(new RegExp(`^0{${SPAN-1}}`), "");
 			console.log("[sanitizedHex]", sanitizedHex);
 			this._data = [...divideText(sanitizedHex, SPAN)];
 		},
